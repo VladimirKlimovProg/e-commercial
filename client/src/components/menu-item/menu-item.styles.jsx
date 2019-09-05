a7a887d99c4924c622a589b35268552394147cac
Vladimir Klimov <vladimirklimoff@yandex.ru>
2019-09-05 15:47:12 +0500

    fixing height bug

--- client/src/components/menu-item/menu-item.styles.jsx
+++ client/src/components/menu-item/menu-item.styles.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const MenuItemContainer = styled.div`
-  height: ${({ size }) => (size ? "380px" : "240px")};
+  height: 40vh;
   min-width: 30%;
   overflow: hidden;
   flex: 1 1 auto;
@@ -27,9 +27,6 @@ export const MenuItemContainer = styled.div`
   }
   &:last-child {
     margin-left: 7.5px;
-  }
-  @media screen and (max-width: 800px) {
-    height: 200px;
   }
 `;
 
